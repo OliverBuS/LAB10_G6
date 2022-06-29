7f4c298d333c674b2ebf48180415b41480dd043e
Oliver <oliver.bustamante@pucp.edu.pe>
2022-06-28 20:40:09 -0500

    Pregunta 3 finalizada

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
 const path =require('path');
-
 const mysql = require('mysql2');
+const bodyparser = require('body-parser');
+const multer = require('multer');
+let upload = multer();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(upload.none());
+
 let conn = mysql.createConnection({
     host:'localhost',
     user:'root',
@@ -20,7 +27,29 @@ app.get('/',function(req,res) {
     res.send('Pagina de inicio');
 });
 
-app.post('/servicio/create/:idmascota',function (req, res){
+app.post('/servicio/create/:idmascota',function (req, res) {
+
+    let parametros = [parseInt(req.params.idmascota), req.body.cuenta_idcuenta, req.body.hora_inicio, req.body.duracion, req.body.entrega, req.body.responsable_idresponsable];
+    let query = "insert into servicio (mascota_idmascota,cuenta_idcuenta,hora_inicio,duracion,entrega,responsable_idresponsable) values (?,?,?,?,?,?)";
+    conn.query(query, parametros, function (err,result){
+        if(err){
+            res.json({
+                status:"error",
+                message:"Ha ocurrido un problema, revise que los parametros sean correctos y que los ids existan"
+            });
+            return;
+        }
+        conn.query("select * from servicio where idservicio=(select last_insert_id())", function (err,result){
+            if(err){
+                res.json({
+                    status:"error",
+                    message:"Ha ocurrido un problema y no se ha subido correctamente el servicio"
+                });
+                return;
+            }
+            res.json(result);
+        })
+    });
 
 });
 

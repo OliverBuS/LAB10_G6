2ca424456964923f24f73bec17f5ea34eb75bfe4
Oliver <oliver.bustamante@pucp.edu.pe>
2022-06-28 19:37:19 -0500

    mysql connector

--- main.js
+++ main.js
@@ -2,8 +2,26 @@ const express = require('express');
 const app = express();
 const path =require('path');
 
+const mysql = require('mysql2');
+let conn = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'root',
+    database:'sandylance'
+});
+
+conn.connect(function (err){
+    if(err) throw err;
+    console.log("Conexión exitosa a la base de datos");
+});
+
+
 app.get('/',function(req,res) {
     res.send('Pagina de inicio');
+});
+
+app.post('/servicio/create/:idmascota',function (req, res){
+
 });
 
 
